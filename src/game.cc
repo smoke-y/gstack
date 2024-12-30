bd5cf27d4d492edb00345af7a42df50f8270e85e
Vish <SnowSneeze@protonmail.com>
2024-12-30 14:07:47 +0530

    updated path

--- src/game.cc
+++ src/game.cc
@@ -2,11 +2,10 @@
     //NOTE
     Clone gstack and add it to .gitignore of your game.
     Game/
-        bin/
         gstack/
         src/
         assets/
         .gitignore
 */
 
-#define GAME_ENTRY "../src/main.cc"
+#define GAME_ENTRY "../../src/main.cc"
